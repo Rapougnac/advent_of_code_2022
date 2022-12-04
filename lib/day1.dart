57b21b907b918ca292f4b892b2ac60d043b6448f
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-12-04 15:44:26 +0100

    Refactor day 1 + add logger

--- lib/day1.dart
+++ lib/day1.dart
@@ -2,44 +2,47 @@ library advent_of_code_2022.day1;
 
 import 'dart:io';
 
+import 'package:logging/logging.dart';
+
+final day1Logger = Logger('Day 1');
+
 void main(List<String> args) {
+  Logger.root.onRecord.listen((event) {
+    print('${DateTime.now()} {Logger: ${event.loggerName}} [${event.level}]: ${event.message}');
+  });
   final elf1 = partOne();
   final elves2 = partTwo();
-  print(elf1);
-  print(elves2);
+  day1Logger.info('Result for part one: $elf1');
+  day1Logger.info('Result for part two: $elves2');
 }
 
-Iterable<List<String>> genElves(String inpt) {
+Iterable<List<String>> genElves([String? inpt]) {
+  inpt ??= input;
   final elvesSplitted = inpt.split('\n\n');
-  final elves = elvesSplitted.map<List<String>>((e) => e.split('\n'));
+  final elves = elvesSplitted.map<List<String>>((e) => e.split('\n')).toList();
+  elves.last.removeLast(); // Remove trailing empty string
   return elves;
 }
 
 int partOne([String? inpt]) {
-  inpt ??= input;
-
   final totalForElves = _(inpt);
 
   return totalForElves.last;
 }
 
 int partTwo([String? inpt]) {
-  inpt ??= input;
-
   final totalForElves = _(inpt);
 
   return totalForElves.reversed.take(3).fold<int>(0, (previousValue, element) => previousValue + element);
 }
 
-List<int> _(String inpt) {
+List<int> _([String? inpt]) {
   final elves = genElves(inpt);
+  day1Logger.finer('Generated elves: $elves');
 
-  final totalForElves = <int>[];
-  for (final calories in elves) {
-    final intParsed = calories.where((e) => e.isNotEmpty).map<int>(int.parse);
-    final total = intParsed.fold<int>(0, (previousValue, element) => previousValue + element);
-    totalForElves.add(total);
-  }
+  final totalForElves = <int>[for (final elf in elves) elf.map<int>(int.parse).fold<int>(0, (pre, el) => pre + el)];
+
+  day1Logger.fine('Parsed elves: $totalForElves');
 
   totalForElves.sort();
 
